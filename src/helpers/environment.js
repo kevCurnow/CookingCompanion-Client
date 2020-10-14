287a81b861015cada54c2ea7a325ed81d80921c1
Kevin Curnow <kevinscurnow@gmail.com>
2020-10-13 21:23:11 -0400

    got heroku hooked up and added static.json

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,6 +4,8 @@ switch(window.location.hostname) {
     case 'localhost' || '127.0.0.1':
     APIURL = 'http://localhost:3000';
     break;
+    case 'ksc-cookingcompanion-client.herokuapp.com':
+    APIURL = 'https://ksc-cookingcompanion-server.herokuapp.com'
 }
 
 export default APIURL;
